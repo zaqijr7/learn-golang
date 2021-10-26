a37ccd926a10b07d0f4cac4bd6e0c6f2cbba4e5e
zaqijr7 <zaqijr7@gmail.com>
2021-10-26 23:31:12 +0700

    learning data type and variable

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	var age int8 = 20
+	var name string = "Muhammad Zaqi Al Quraisyi"
+	university := "Asia Cyber University"
 	println("Hello world")
+
+	fmt.Println("Hello my name is ", name)
+	fmt.Println("I am", age, "years old")
+	fmt.Println("Currently i study of", university)
+	{
+		name := "Aaz"
+		println("Overide variable value of name : ", name)
+	}
+	println("scope of variable in golang", name)
 }
